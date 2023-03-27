0ca1fc67c423ff353fc0498b824f664f22bf14f8
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-03-27 17:25:09 +0400

    localStorage and deleting tasks

--- src/components/Todo/List.jsx
+++ src/components/Todo/List.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
-const List = ({ items, onChangeItem }) => {
+const List = ({ items, onChangeItem, onRemoveItem }) => {
   return (
     <div className='view-sm flex-col'>
       {items && items.length > 0
-        ? items.map((item, index) => (
-            <div key={item.id} className='ui-checkbox'>
-              <input
-                id={item.id}
-                type='checkbox'
-                checked={item.isChecked}
-                onChange={() => onChangeItem(item.id)}
-              />
-              <label htmlFor={item.id}>{item.title}</label>
+        ? items.map((item) => (
+            <div  key={item.id} className='flex justify-between items-center mb-2'>
+              <div className='ui-checkbox'>
+                <input
+                  id={item.id}
+                  type='checkbox'
+                  checked={item.isChecked}
+                  onChange={() => onChangeItem(item.id)}
+                />
+                <label htmlFor={item.id}>{item.title}</label>
+              </div>
+              <span className='ui-link pl-2' onClick={()=> onRemoveItem(item.id)}>Remove</span>
             </div>
           ))
         : 'items not found'}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,45 @@
 import React, { useState } from 'react'
-import { Form, List } from  '../../components/Todo/'
+import { Form, List } from '../../components/Todo/'
 import { Container } from '../../layouts'
 
 const HomePage = () => {
-  const [items, setItems] = useState([])
+  const localItem = JSON.parse(localStorage.getItem('items'))
+  const [items, setItems] = useState(localItem || [])
   const handleSubmit = (item) => {
     const nextItems = [...items, item]
     setItems(nextItems)
   }
 
   const handleChangeItem = (id) => {
-    const nextItems = items.map((el) => el.id == id ? {...el, isChecked: !el.isChecked} : el)
+    const nextItems = items.map((el) =>
+      el.id == id ? { ...el, isChecked: !el.isChecked } : el
+    )
+    setItems(nextItems)
+  }
+
+  const handleRemoveItem = (id) => {
+    // console.log(`removed : ${id}` )
+    const nextItems = [...items]
+    const indexForRemove = nextItems.findIndex((el) => el.id === id)
+    // console.log(indexForRemove)
+    nextItems.splice(indexForRemove, 1)
     setItems(nextItems)
 
   }
 
-  React.useEffect(() => console.log(items), [items])
+  React.useEffect(() => {
+    // console.log(items),
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
 
   return (
     <Container>
       <Form onSubmit={handleSubmit} />
-      <List items={items} onChangeItem={handleChangeItem}/>
+      <List
+        items={items}
+        onChangeItem={handleChangeItem}
+        onRemoveItem={handleRemoveItem}
+      />
     </Container>
   )
 }
