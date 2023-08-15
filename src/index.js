4a06e9ae904434291288f2cd96cb58a2ec6329d9
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-15 17:51:13 +0400

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/todo-list">
       <App />
     </BrowserRouter>
   </React.StrictMode>
